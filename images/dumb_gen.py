50b0aa24562aa364ce02a00b50bd0d247d7ad98b
clach04 <clach04@gmail.com>
2024-11-27 22:37:17 -0800

    screenshot preview

---
+++ images/dumb_gen.py
@@ -0,0 +1,16 @@
+
+# py -3 dumb_gen.py > README.md
+
+import glob
+
+flist = glob.glob('*.png')
+flist.sort()
+
+print('''# Screenshots
+
+''')
+for fname in flist:
+    print('''`%s`
+
+![image](%s)
+''' % (fname, fname, ))
